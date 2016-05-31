13daacf78319d6f3c331e7324e0ccfc4ac4512b3
sushmithaleela <sushmitha.leela@gmail.com>
2016-05-31 16:35:37 +0530

    first commit

---
+++ AutotoolsAsst/src/func.h
@@ -0,0 +1,14 @@
+/*
+ * func.h
+ *
+ *  Created on: 31-May-2016
+ *      Author: sushmitha
+ */
+
+#ifndef FUNC_H_
+#define FUNC_H_
+#include<stdio.h>
+ int revSpiral();
+
+
+#endif /* FUNC_H_ */
---
+++ AutotoolsAsst/src/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: 31-May-2016
+ *      Author: sushmitha
+ */
+
+#include "func.h"
+
+int main(){
+	revSpiral();
+	return 0;
+}
+
